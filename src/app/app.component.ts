8ec89fe544586290545c99d38f952bc7fe068e11
leahmkh <105560625+leahmkh@users.noreply.github.com>
2024-10-11 10:15:40 -1000

    router repair

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,9 +15,9 @@ import { RouterOutlet, RouterLink } from '@angular/router';
       <nav class="navbar">
        <ul class="navbar-list">
         <li class="navbar-item"><a routerLink="/">Home</a></li>
-        <li class="navbar-item"><a href="/about">About</a></li>
-        <li class="navbar-item"><a href="/resume">Resume</a></li>
-        <li class="navbar-item"><a href="/projects">Projects</a></li>
+        <li class="navbar-item"><a routerLink="/about">About</a></li>
+        <li class="navbar-item"><a routerLink="/resume">Resume</a></li>
+        <li class="navbar-item"><a routerLink="/projects">Projects</a></li>
        </ul>
       </nav>
    
@@ -30,9 +30,9 @@ import { RouterOutlet, RouterLink } from '@angular/router';
    <footer class="footer">
     <nav class="footer-nav">
      <a routerLink="/">Home</a> |
-     <a href="/about">About</a> |
-     <a href="/resume">Resume</a> |
-     <a href="/projects">Projects</a>
+     <a routerLink="/about">About</a> |
+     <a routerLink="/resume">Resume</a> |
+     <a routerLink="/projects">Projects</a>
     </nav>
     <p>&copy; 2024</p>
    </footer>

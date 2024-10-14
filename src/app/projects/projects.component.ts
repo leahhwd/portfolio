e62533dd13f893f6f796c929adf3e4dcbef5d14d
leahmkh <105560625+leahmkh@users.noreply.github.com>
2024-10-13 15:21:56 -1000

    projects update

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -5,9 +5,49 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      projects works!
-    </p>
+    <div class="projects-container">
+     <div class="project">
+      <h2>In-N-Out Books</h2>
+      <p class="description">
+       A project using Node.js and Express with a Mock NoSQL database to make API calls.
+      </p>
+      <div class="technology">
+       <h4>Technologies:</h4>
+       <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>JavaScript</li>
+       </ul>
+      </div>
+       <a href="https://github.com/leahhwd/in-n-out" target="_blank">
+       GitHub Repository
+       </a>
+     </div>
+
+     <div class="project">
+      <h2>RPG Character Builder</h2>
+      <p class="description">
+       A project using Angular to create characters in a Single Page Application (SPA).
+      </p>
+      <div class="technology">
+       <h4>Technologies</h4>
+       <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Angular</li>
+        <li>JavaScript</li>
+        <li>TypeScript</li>
+       </ul>
+      </div>
+       <a href="https://github.com/leahhwd/web-425/tree/main/rpg-character-builder" target="_blank">
+        GitHub Repository
+       </a>
+     </div>
+    </div>
+
+
   `,
   styles: ``
 })
